de72a83cbac7f00f395a91268b5b2cad9094b34a
Ahmed Fouad <fouad.bhatti@gmail.com>
2018-02-24 03:11:12 +0500

    Bug fix fetch icons assets

--- js/config.js
+++ js/config.js
@@ -3,5 +3,5 @@ const Config = {
 	secondary: '',
 	apiKey: 'apiKey=freekey',
 	cmcTicker: 'https://api.coinmarketcap.com/v1/ticker/?limit=0',
-	cmcIcons: 'https://files.coinmarketcap.com/static/img/coins/64x64/'
+	cmcIcons: 'https://coincodex.com//en/resources/images/admin/coins/'
 };
