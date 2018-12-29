92051e3b5566ab8ee6ced6cac8f4f211cb4eef5f
Ahmed Fouad <fouadbhatti@users.noreply.github.com>
2018-12-30 02:26:47 +0500

    Workaround cmc changed api limit to 100

--- js/cmcTicker.js
+++ js/cmcTicker.js
@@ -1,11 +1,21 @@
 class CmcTicker {
 	static getTicker() {
-		return Utils.request({
+		let first100$ = Utils.request({
 			method: 'GET',
-			url: Config.cmcTicker,
+			url: `${Config.cmcTicker}&start=0`,
 		})
 		.catch(e => {
 			return Rx.Observable.of(null);
+		});
+		
+		let second100$ = Utils.request({
+			method: 'GET',
+			url: `${Config.cmcTicker}&start=100`
 		})
+		.catch(e => {
+			return Rx.Observable.of(null);
+		});
+		
+		return Rx.Observable.forkJoin([first100$, second100$]);
 	}
 }
