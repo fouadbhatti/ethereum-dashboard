613ee0fe6a3899c2182513022c9df05c80dc7dbf
Ahmed Fouad <fouadbhatti@users.noreply.github.com>
2018-12-30 02:33:29 +0500

    Destructure ticker

--- js/wallets.js
+++ js/wallets.js
@@ -30,6 +30,7 @@ class MyWallets {
 					let $wallets = Rx.Observable.of(wallets);
 					let $cmcTicker = CmcTicker.getTicker();
 					return Rx.Observable.zip($wallets, $cmcTicker, (wallets, ticker) => {
+						ticker = [...ticker[0], ...ticker[1]];
 						return wallets.map(wallet => {
 								if (wallet && wallet.tokens) {
 									let tokens =  wallet.tokens.map(token => {
