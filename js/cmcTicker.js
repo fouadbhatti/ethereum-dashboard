732c3f5a67804d4738ce41ad703c10b9713c04e5
Ahmed Fouad <fouadbhatti@users.noreply.github.com>
2018-12-30 02:44:05 +0500

    Updated cmcTicker added more pages

--- js/cmcTicker.js
+++ js/cmcTicker.js
@@ -16,6 +16,30 @@ class CmcTicker {
 			return Rx.Observable.of(null);
 		});
 		
-		return Rx.Observable.forkJoin([first100$, second100$]);
+		let third100$ = Utils.request({
+			method: 'GET',
+			url: `${Config.cmcTicker}&start=200`
+		})
+		.catch(e => {
+			return Rx.Observable.of(null);
+		});
+		
+		let fourth100$ = Utils.request({
+			method: 'GET',
+			url: `${Config.cmcTicker}&start=300`
+		})
+		.catch(e => {
+			return Rx.Observable.of(null);
+		});
+		
+		let fifth100$ = Utils.request({
+			method: 'GET',
+			url: `${Config.cmcTicker}&start=400`
+		})
+		.catch(e => {
+			return Rx.Observable.of(null);
+		});
+		
+		return Rx.Observable.forkJoin([first100$, second100$, third100$, fourth100$, fifth100$]);
 	}
 }
