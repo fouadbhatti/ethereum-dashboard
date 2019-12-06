ac1ffa67e2f044cb74f92147f48978ba76240044
Ahmed Fouad <fouadbhatti@users.noreply.github.com>
2019-12-06 22:19:51 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ const Config = {
 	primary: 'https://api.ethplorer.io/getAddressInfo',
 	secondary: '',
 	apiKey: 'apiKey=igei3425wgGmM73',
-	cmcTicker: 'https://api.coinmarketcap.com/v1/ticker/?limit=0',
+	cmcTicker: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=a7e67b45-3482-49fb-92dc-1080d5b30ac7',
 	cmcIcons: 'https://coincodex.com//en/resources/images/admin/coins/'
 };
